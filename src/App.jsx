224d36acd418eafe1d78b4ce4e30bca1ce3c4d0d
Rajat Rahangdale <rjtrahangdale@gmail.com>
2024-08-06 10:19:11 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import LoginPage from "./Login/Components/LoginPage";
 
 const App = () => {
-  return <div className="text-2xl">App</div>;
+  return (
+    <div className="flex w-[1440px] h-[960px] bg-slate-600">
+      {/* image panel */}
+      <div className="w-[874px] h-[975px] bg-slate-300">image</div>
+      <div>
+        <LoginPage />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Login/Components/MobileSignUpPage.jsx
@@ -0,0 +1,5 @@
+const MobileSignUpPage = () => {
+  return <div>MobileSignUpPage</div>;
+};
+
+export default MobileSignUpPage;
